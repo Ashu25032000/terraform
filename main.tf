13794f31b28318f565cde3eabb0e4cc32f8a10ec
Ashwini <ashwinibhavsar871@gmail.com>
2025-09-05 17:50:46 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Configure the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  profile = "terraform"
+}
+
+# # Create an EC2 instance
+# resource "aws_instance" "example" {
+#   ami           = "<ami_name>"
+#   instance_type = "<instance_type_name>"
+# }
