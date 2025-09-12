5bbf1a0330989c9f4dda698723fb4b5cedecd4ce
Ashwini <ashwinibhavsar871@gmail.com>
2025-09-12 15:57:15 +0530

    updating

--- 01-Introduction/main.tf
+++ 01-Introduction/main.tf
@@ -3,16 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67.0"
+      version = "6.10.0"
     }
   }
-  }
+}
 
 provider "aws" {
   region = "ap-south-1"
+  profile = "terraform"
 }
-
-
 
 
 # # Create an EC2 instance
--- 02-Working_with_EC2/main.tf
+++ 02-Working_with_EC2/main.tf
@@ -1,15 +1,15 @@
-# Configure the AWS provider
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67.0"
+      version = "6.10.0"
     }
   }
 }
+
 provider "aws" {
   region = "ap-south-1"
-  profile = "<profile_name>"
+  profile = "terraform"
 }
 
 # Create an EC2 instance
